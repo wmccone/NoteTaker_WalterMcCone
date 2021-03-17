9dcb399e9401a0d9176f929649acfeaceb1d9c00
Gerry M <waltermccone88@gmail.com>
2021-03-16 20:47:02 -0500

    created a working server and created the base routes

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const dataBase= require('./db/db.json')
+
+// Sets up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//(DATA) This code will store the note objects
+
+const notes = [
+
+]
+
+
+// Basic route sends the user the HTML files
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+// Displays the notes
+app.get('/api/notes', (req, res) => res.json(dataBase));
+
+//Posts new data to the server
+
+app.post('/api/notes', (req, res) => {
+    console.log(`Adding ${req.body}`)
+    // dataBase.push(req.body)
+});
+
+app.delete('/api/notes', (req, res) => {
+    console.log(`Deleting ${req.body}`)
+});
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
